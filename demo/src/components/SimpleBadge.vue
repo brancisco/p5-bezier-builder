36210dcba96ddf217e34db10019055e2122ce340
Brandon Aguirre <brag4918@colorado.edu>
2020-11-10 15:35:22 -0700

    a simple badge component

---
+++ demo/src/components/SimpleBadge.vue
@@ -0,0 +1,25 @@
+<template>
+    <span class="simple-badge">{{text}}</span>
+</template>
+
+<script>
+    export default {
+        name: "SimpleBadge",
+        props: {
+            text: String | Number
+        },
+    }
+</script>
+
+<style scoped>
+    .simple-badge {
+        text-align: center;
+        background: #2574a9;
+        color: white;
+        border-radius: 10px;
+        min-width: 20px;
+        padding: 1px 8px;
+        display: inline-block;
+        font-size: 0.8rem;
+    }
+</style>
